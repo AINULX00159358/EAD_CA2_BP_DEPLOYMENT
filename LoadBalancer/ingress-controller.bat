c26be0d9d54b002fa3cea17e2cd6f36360ffcced
ainulhabib <ainul.habib@paddypowerbetfair.com>
2023-04-22 14:05:31 +0100

    added ingress-controller.bat

---
+++ LoadBalancer/ingress-controller.bat
@@ -0,0 +1,6 @@
+set NAMESPACE=ingress-basic
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace %NAMESPACE% --set controller.service.annotations."service^.beta^.kubernetes^.io/azure-load-balancer-health-probe-request-path"=/health
