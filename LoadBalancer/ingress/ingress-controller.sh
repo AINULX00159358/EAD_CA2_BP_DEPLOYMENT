ebcd42ffe215ee48ff886991c7d93d673dbf678d
ainulhabib <ainul.habib@paddypowerbetfair.com>
2023-04-25 10:49:19 +0100

    Added all deployment Pipelines

---
+++ LoadBalancer/ingress/ingress-controller.sh
@@ -0,0 +1,5 @@
+NAMESPACE=ingress-basic
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm uninstall ingress-nginx -n ${NAMESPACE}
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace ${NAMESPACE} --set controller.service.annotations."service.beta.kubernetes.io/azure-load-balancer-health-probe-request-path"=/healthz
